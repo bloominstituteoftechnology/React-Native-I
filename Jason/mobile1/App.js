484ea05fd558fdc0c7dd72d5f1036dedae1fb364
JCamp-12 <username@users.noreply.github.com>
2017-09-08 19:44:41 -0400

    1st ok styled component

--- Jason/mobile1/App.js
+++ Jason/mobile1/App.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() { // not using right now, but just like React
+    super();
+    this.state = {
+
+    };
+  }
   render() {
-    console.log('Hello there')
-    return (
+    console.log('Well, hello there!')
+    return ( // the JSX is different than in React
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text>
+        <Text>This is totally cool!</Text> */}
+        <View style={styles.child}>
+          <Text style={styles.text1}>
+            Like
+          </Text>
+        </View>
+        <View style={styles.child}>
+          <Text style={styles.text2}>
+            Share
+          </Text>
+        </View>
+        <View style={styles.child}>
+          <Text style={styles.text3}>
+            Comment
+          </Text>
+        </View>
       </View>
     );
   }
@@ -15,8 +37,27 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    display: 'flex',
+    backgroundColor: 'whitesmoke',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
+  child: {
+    height: 30,
+    width: 75,
+    backgroundColor: 'salmon',
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  text1: {
+    textAlign: 'center',
+  },
+  text2: {
+    textAlign: 'center',
+  },
+  text3: {
+    textAlign: 'center',
+  }
 });

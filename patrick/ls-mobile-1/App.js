5c9cb9fc0b204436d4a512e8a9854f7f1d6cf8b8
mixelpixel <mixelpix@mac.com>
2017-09-10 06:03:55 -0400

    CSS module

--- patrick/ls-mobile-1/App.js
+++ patrick/ls-mobile-1/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './CSS/stylesheet'; // <~~~~ importing the CSS
 
 export default class App extends React.Component {
   constructor() { // not using right now, but just like React
@@ -51,39 +52,39 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    backgroundColor: 'whitesmoke',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  child: {
-    height: 50,
-    width: 150,
-    marginBottom: 10,
-    backgroundColor: 'salmon',
-    // alignItems: 'center',
-    justifyContent: 'center', // <~~~~ that's how.
-  },
-  child2: {
-    height: 50,
-    width: 150,
-    marginBottom: 10,
-    backgroundColor: 'salmon',
-    // alignItems: 'center',
-    justifyContent: 'center', // <~~~~ that's how.
-  },
-  text1: {
-    // alignSelf: 'center',
-    textAlign: 'center',
-    // textAlignVertical: 'center', // <~~~~ HOW?????
-  },
-  text2: {
-    textAlign: 'auto',
-  },
-  text3: {
-    textAlign: 'justify',
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     display: 'flex',
+//     backgroundColor: 'whitesmoke',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   child: {
+//     height: 50,
+//     width: 150,
+//     marginBottom: 10,
+//     backgroundColor: 'salmon',
+//     // alignItems: 'center',
+//     justifyContent: 'center', // <~~~~ that's how.
+//   },
+//   child2: {
+//     height: 50,
+//     width: 150,
+//     marginBottom: 10,
+//     backgroundColor: 'salmon',
+//     // alignItems: 'center',
+//     justifyContent: 'center', // <~~~~ that's how.
+//   },
+//   text1: {
+//     // alignSelf: 'center',
+//     textAlign: 'center',
+//     // textAlignVertical: 'center', // <~~~~ HOW?????
+//   },
+//   text2: {
+//     textAlign: 'auto',
+//   },
+//   text3: {
+//     textAlign: 'justify',
+//   }
+// });
---
+++ patrick/ls-mobile-1/CSS/stylesheet.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    backgroundColor: 'whitesmoke',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  child: {
+    height: 50,
+    width: 150,
+    marginBottom: 10,
+    backgroundColor: 'salmon',
+    // alignItems: 'center',
+    justifyContent: 'center', // <~~~~ that's how.
+  },
+  child2: {
+    height: 50,
+    width: 150,
+    marginBottom: 10,
+    backgroundColor: 'salmon',
+    // alignItems: 'center',
+    justifyContent: 'center', // <~~~~ that's how.
+  },
+  text1: {
+    // alignSelf: 'center',
+    textAlign: 'center',
+    // textAlignVertical: 'center', // <~~~~ HOW?????
+  },
+  text2: {
+    textAlign: 'auto',
+  },
+  text3: {
+    textAlign: 'justify',
+  }
+});

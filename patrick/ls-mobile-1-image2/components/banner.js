43a981090327bc697dc933267a52667f52c5fb11
mixelpixel <mixelpix@mac.com>
2017-09-10 23:05:44 -0400

    idem

--- patrick/ls-mobile-1-image2/components/banner.js
+++ patrick/ls-mobile-1-image2/components/banner.js
@@ -8,13 +8,22 @@ export default class Banner extends React.Component {
   render() {
     return (
       <View style={styles.banner}>
-        <Text>Text from the BANNER component.</Text>
+        {/* <Text>Text from the BANNER component.</Text> */}
         <Image
           // source={require('../media/9_8_PM_meeting.png')}
           source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/cat.gif' }}
-          style={{ height: height / 5, width: width }}
+          style={{ height: height / 5, width: width / 2 }}
           // resizeMode='contain'
           // resizeMode='cover'
+        />
+        <Image
+          source={require('../media/9_8_PM_meeting.png')}
+          // source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/cat.gif' }}
+          // style={{ height: height / 5, width: width / 2 }}
+          // resizeMode='contain'
+          // resizeMode='cover'
+          // resizeMode='stretch'
+          style={styles.stretch}
         />
       </View>
     );
@@ -22,9 +31,14 @@ export default class Banner extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  stretch: {
+    height: height / 5,
+    width: width / 2
+  },
   banner: {
     borderWidth: 1,
     borderColor: 'blue',
     backgroundColor: 'gold',
+    flexDirection: 'row',
   },
 });

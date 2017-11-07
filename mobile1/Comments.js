372735b987f06132f3eca9474b113ce0bfc39723
Neil Barduson <barduson.neil@gmail.com>
2017-11-07 14:51:22 -0600

    only finished the design one, but got fundamentals of react native and flexbox

---
+++ mobile1/Comments.js
@@ -0,0 +1,26 @@
+// import React from 'react'
+
+// class Comment extends React.Component {
+// 	constructor() {
+// 	  super();
+// 	  this.state = {
+// 	    Comments: []
+// 	  }
+// 	}
+
+// 	// once the app component add loads (mounts), set the people to the imported people.js
+// 	// this is where you instantiate the components data
+// 	componentDidMount() {
+// 	  this.setState({Comments: ['cute!! totez amazing', 'prescient and prudent', 'meeeoowww!']});
+// 	}
+
+// 	render() {
+// 	  return (
+// 	      {this.state.Comments.map((comment => {
+// 	        return(
+// 	        	<Text>{comment}</Text>
+// 	        )
+// 	      }))}
+// 	  );
+// 	}
+// }

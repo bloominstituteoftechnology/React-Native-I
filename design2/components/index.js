e01061c9cc9ccef8c9116d6be5f92645a00f5e32
Adam Fetters <adamfetterscoder@gmail.com>
2018-02-07 10:43:18 -0500

    Deleted corrupt Design2 folder / Replace with Design2Redo

--- design2/components/index.js
+++
@@ -1,7 +0,0 @@
-import TopContainer from './TopContainer';
-import Header from './Header';
-import BannerImageContainer from './BannerImageContainer';
-import SmallerImageContainer from './SmallerImageContainer';
-import InfoContainer from './InfoContainer';
-
-export { TopContainer, Header, BannerImageContainer, SmallerImageContainer, InfoContainer };
